a022a5025229b5f3b5e1ff6b11fcab8beb5f64d6
norda97 <adde--97@hotmail.com>
2019-10-15 15:41:00 +0200

    added node

---
+++ src/wumpusworld/Imp/Node.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wumpusworld.Imp;
+
+import java.util.List;
+import java.util.ArrayList;
+import wumpusworld.Imp.Vector2;
+/**
+ *
+ * @author Adrian Nordin, Jonathan Åleskog, Daniel Cheh
+ */
+public class Node {
+    public List<Node> neighbours;
+    public Vector2 index;
+   
+    public Node(int x, int y) {
+        this.index = new Vector2(x, y);
+        this.neighbours = new ArrayList();
+    }
+    
+    public void addNeighbour(Node n) {
+        this.neighbours.add(n);
+    }
+}
